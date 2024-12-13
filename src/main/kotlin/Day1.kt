52d44f9635c03e1087d9bb12b99f16ad28270e80
Todd Riley <todd@websbytodd.com>
2024-12-13 11:15:08 -0500

    Part 1 works.

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,9 +1,49 @@
 import java.io.File
+import kotlin.math.abs
 
-class Day1 {
-  fun read() {
-    val fileName = "src/main/resources/day1-test.txt"
+class Day1(private val fileName: String) {
+
+  private val leftList = mutableListOf<Int>()
+  private val rightList = mutableListOf<Int>()
+  private var isDebug = false
+
+  private fun readInput() {
     val file = File(fileName)
-    file.forEachLine { println(it) }
+    printDebug("*****INPUT******")
+    file.forEachLine {
+      printDebug(it)
+      val numbers = it.split("\\s+".toRegex()).map { it.toInt() }
+      leftList.add(numbers[0])
+      rightList.add(numbers[1])
+    }
+    printDebug("*****INPUT******")
+  }
+
+  fun enableDebug() {
+    isDebug = true
+  }
+
+  fun printDebug(message: String) {
+    if (isDebug) {
+      println(message)
+    }
+  }
+
+  fun go() {
+    readInput()
+    leftList.sort()
+    rightList.sort()
+    var totalDistance = 0
+    for (i in 0..<leftList.size) {
+      val leftNum = leftList[i]
+      val rightNum = rightList[i]
+      val distance = abs(rightNum - leftNum)
+      totalDistance += distance
+    }
+    println("******************************")
+    println("******************************")
+    println("Total Distance is $totalDistance")
+    println("******************************")
+    println("******************************")
   }
 }
--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -2,10 +2,9 @@ import kotlin.test.Test
 
 class Day1Test {
 
-  private val day1: Day1 = Day1()
-
   @Test
   fun testRead() {
-    day1.read()
+    val day1 = Day1("src/main/resources/day1-part1.txt")
+    day1.go()
   }
 }
