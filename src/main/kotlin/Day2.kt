fde880dba28451c293984a6b5f3ea1a30f0f626e
Todd Riley <todd@websbytodd.com>
2024-12-13 16:01:44 -0500

    Day 2 part 1 reactive.

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -4,32 +4,29 @@ import kotlin.math.abs
 class Day2(private val fileName: String) {
   fun part1(): Int {
     val file = File(fileName)
-    var safeReportCount = 0;
+    var safeReportCount = 0
     file.forEachLine { line ->
-      val report = line.split("\\s+".toRegex()).map { it.toInt() }
-      var direction = 0
-      var isSafe = true
-      report.zipWithNext({ a, b ->
-        val diff = a - b
-        if (abs(diff) > 3 || diff == 0) {
-          isSafe = false
-        } else if (diff < 0) {
-          if (direction == 1) {
-            isSafe = false
-          } else {
-            direction = -1
+      line
+          .split("\\s+".toRegex())
+          .map { it.toInt() }
+          .zipWithNext { a, b -> false to a - b }
+          .reduce { acc, i ->
+            if (acc.first) {
+              return@reduce acc
+            }
+            val prevDiff = acc.second
+            val nextDiff = i.second
+            val isUnsafe =
+                abs(prevDiff) > 3 ||
+                    abs(nextDiff) > 3 ||
+                    prevDiff == 0 ||
+                    nextDiff == 0 ||
+                    (prevDiff < 0 && nextDiff > 0) ||
+                    (prevDiff > 0 && nextDiff < 0)
+            isUnsafe to prevDiff
           }
-        } else {
-          if (direction == -1) {
-            isSafe = false
-          } else {
-            direction = 1
-          }
-        }
-      })
-      if (isSafe) {
-        safeReportCount++
-      }
+          .let { if (!it.first) safeReportCount++ }
+      println("safeReportCount: $safeReportCount $line")
     }
     return safeReportCount
   }
