c88a6936901739faa3bde8bfa9faf5ef820b84cf
Todd Riley <todd@websbytodd.com>
2024-12-13 16:30:25 -0500

    Day 2 improved.

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,33 +1,28 @@
 import java.io.File
 import kotlin.math.abs
+import kotlin.math.sign
 
 class Day2(private val fileName: String) {
   fun part1(): Int {
     val file = File(fileName)
     var safeReportCount = 0
     file.forEachLine { line ->
-      line
-          .split("\\s+".toRegex())
-          .map { it.toInt() }
-          .zipWithNext { a, b -> false to a - b }
-          .reduce { acc, i ->
-            if (acc.first) {
-              return@reduce acc
-            }
-            val prevDiff = acc.second
-            val nextDiff = i.second
-            val isUnsafe =
-                abs(prevDiff) > 3 ||
-                    abs(nextDiff) > 3 ||
-                    prevDiff == 0 ||
-                    nextDiff == 0 ||
-                    (prevDiff < 0 && nextDiff > 0) ||
-                    (prevDiff > 0 && nextDiff < 0)
-            isUnsafe to prevDiff
-          }
-          .let { if (!it.first) safeReportCount++ }
+      val isSafe =
+          line
+              .split("\\s+".toRegex())
+              .map { it.toInt() }
+              .zipWithNext { a, b -> a - b }
+              .zipWithNext { diffA, diffB -> diffIsSafe(diffA, diffB) }
+              .all { it }
+      if (isSafe) safeReportCount++
       println("safeReportCount: $safeReportCount $line")
     }
     return safeReportCount
+  }
+
+  private fun diffIsSafe(diffA: Int, diffB: Int): Boolean {
+    return diffA.sign == diffB.sign &&
+        abs(diffA).let { it == 1 || it == 2 || it == 3} &&
+        abs(diffB).let { it == 1 || it == 2 || it == 3}
   }
 }
