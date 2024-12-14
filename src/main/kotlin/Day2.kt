dcc4b7874ba50d1398973239f915a496486249ad
Todd Riley <todd@websbytodd.com>
2024-12-13 20:16:39 -0500

    Day 2 part 2 works.

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -12,17 +12,39 @@ class Day2(private val fileName: String) {
               .split("\\s+".toRegex())
               .map { it.toInt() }
               .zipWithNext { a, b -> a - b }
-              .zipWithNext { diffA, diffB -> diffIsSafe(diffA, diffB) }
+              .zipWithNext(::diffIsSafe)
               .all { it }
       if (isSafe) safeReportCount++
-      println("safeReportCount: $safeReportCount $line")
     }
     return safeReportCount
   }
 
   private fun diffIsSafe(diffA: Int, diffB: Int): Boolean {
     return diffA.sign == diffB.sign &&
-        abs(diffA).let { it == 1 || it == 2 || it == 3} &&
-        abs(diffB).let { it == 1 || it == 2 || it == 3}
+        abs(diffA).let { it == 1 || it == 2 || it == 3 } &&
+        abs(diffB).let { it == 1 || it == 2 || it == 3 }
+  }
+
+  fun part2(): Int {
+    val file = File(fileName)
+    var safeReportCount = 0
+    file.forEachLine { line ->
+      val report = line.split("\\s+".toRegex()).map { it.toInt() }
+      val isSafe = report.zipWithNext { a, b -> a - b }.zipWithNext(::diffIsSafe).all { it }
+      if (isSafe) {
+        safeReportCount++
+      } else {
+        for (i in 0 until report.size) {
+          val trimmedReport = report.filterIndexed { index, _ -> index != i }
+          val isSafe =
+              trimmedReport.zipWithNext { a, b -> a - b }.zipWithNext(::diffIsSafe).all { it }
+          if (isSafe) {
+            safeReportCount++
+            break
+          }
+        }
+      }
+    }
+    return safeReportCount
   }
 }
--- src/test/kotlin/Day2Test.kt
+++ src/test/kotlin/Day2Test.kt
@@ -6,7 +6,10 @@ class Day2Test {
     fun part1() {
         val day2Test = Day2("src/main/resources/day2-test.txt")
         expectSelfie(day2Test.part1()).toBe(2)
+        expectSelfie(day2Test.part2()).toBe(4)
         val day2 = Day2("src/main/resources/day2.txt")
         expectSelfie(day2.part1()).toBe(479)
+        expectSelfie(day2.part2()).toBe(531)
     }
+
 }
