f04a898c981f86691ee97b1698b1a49bf3841598
Todd Riley <todd@websbytodd.com>
2024-12-16 11:14:11 -0500

    Day 3 part 2. Works for test input, but not for full input.

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -7,16 +7,31 @@ class Day3(private val fileName: String) {
     file.forEachLine { line ->
       val regex = Regex("mul\\(\\d+,\\d+\\)")
       val matches = regex.findAll(line)
-      sum +=
-          matches
-              .map {
-                it.value.substringAfter("mul(").substringBefore(")").split(",").zipWithNext { a, b
-                  ->
-                  a.toInt() * b.toInt()
-                }
-              }
-              .flatten()
-              .sum()
+      sum += matches.map(::mul).sum()
+    }
+    return sum
+  }
+
+  private fun mul(mulStr: MatchResult): Int =
+      mulStr.value.substringAfter("mul(").substringBefore(")").split(",").zipWithNext { a, b ->
+        a.toInt() * b.toInt()
+      }[0]
+
+  fun part2(): Int {
+    val file = File(fileName)
+    var sum = 0
+    file.forEachLine { line ->
+      val regex = Regex("""mul\(\d+,\d+\)|do\(\)|don't\(\)""")
+      val matches = regex.findAll(line)
+      var isCounting = true
+      for (match in matches) {
+        val value = match.value
+        when (value) {
+          "don't()" -> isCounting = false
+          "do()" -> isCounting = true
+          else -> if (isCounting) sum += mul(match)
+        }
+      }
     }
     return sum
   }
--- src/test/kotlin/Day3Test.kt
+++ src/test/kotlin/Day3Test.kt
@@ -6,7 +6,10 @@ class Day3Test {
     fun part1() {
         val day3Test = Day3("src/main/resources/day3-test.txt")
         expectSelfie(day3Test.part1()).toBe(161)
+        val day3Test2 = Day3("src/main/resources/day3-test2.txt")
+        expectSelfie(day3Test2.part2()).toBe(48)
         val day3 = Day3("src/main/resources/day3.txt")
         expectSelfie(day3.part1()).toBe(168_539_636)
+        expectSelfie(day3.part2()).toBe(102_631_226)
     }
 }
